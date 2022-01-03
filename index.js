d3f38c683bf5f33b852f2dc443a3d1aa1e99f4da
Steven Jankowski <steven.jankowski1@gmail.com>
2022-01-03 13:13:02 +0000

    commit tree 53daa9890c7cf6fe5cff99bc741e9cc1147f634e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+let submitData = {
+  name: "Steven",
+  email: "steven.jankowski@gmail.com",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(submitData),
+};
+
+function submitData(name, email) {
+  fetch("http://localhost:3000/users", configurationObject);
+}
